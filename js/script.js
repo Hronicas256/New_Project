f46888cf44f5ac1ab01650f76cad106da3051c10
Kostya <hronicas256@gmail.com>
2020-09-10 13:38:17 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцениет его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцениет его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
